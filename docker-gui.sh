46dc10125ea375b1027711f42b4bacd62b436fdc
Myuki <myuki@outlook.ie>
2023-04-09 01:10:17 +0800

    Update docker-gui.sh

--- docker-gui.sh
+++ docker-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker create -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e TZ $*
+docker create -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY $*
